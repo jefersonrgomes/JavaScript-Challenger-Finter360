f8d1ba4ac3b8fa865846801cf3396464f4af695e
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-02-24 04:31:36 -0300

    fun Repaced tested

---
+++ tester.js
@@ -0,0 +1,128 @@
+const txtB =
+  'lqvxj prbf dgxv cgc wqvxbjs dbs msvtlbr frsm wbdqdg xhpjp jzfcchbs qjq zqtcjlkm slt tzbc nsr wjhzxd rffqbkm dvfn gsvfgb blxxngsh zchhpqg dxmk zwngclz cgv tgxznzlg pvdnnx rnbj vwx xhrzg zdq bjqdhkps dmpb gkk vfr whmxrbm pngfqc nlbd xgm cbpqw vvgxpv vzbbgx jlmtdcdk xvrmvkmt zbkvzjx bslb dtzwvk jsvk qqr llp mxhjqck gkhmf xvqfqcjj ffvx zvmqpsm sxx vtbhc nhfg gcprb nxlctr dsbnb kwggfpw tqm fnvpsl vxdpzb ljcfrxg rzq mfq hgrwcx pbktg nkslbdgc rslj xkrx hmdrsk nhf wwrqwtz lfqqjzw qdcvzvjc kdmmh wqnnkmcm qkwmdxlf mctc xkv nrbx lbdj tdxgbb hpv gzvgndnb lhsvjxdd nvf mvlpsghr skvtfpw fjjxcp dvf dnjdzcrc bkv qsqplh rxcw tfnwhw lbttltk kphjcw sgmx tdgjvqdb pmvhqzf rzhb fkxmsdw svqcpq hhx pprkm shh lfrstvcd rdkdqqq ddhgdcr tsk fmktwj trv ltwnhthz nml tsl kpzgksnh ndz qtqglj dbwmdvh wgrvdklq csnlxvnj dcnzgs wwzr ngtc vppg fbvsdr vmx rkzcgw jjglmvb rpd rvdgxtnr xqgshj vclcmnh smbhg xmjlklzl mcbz slkbsj xbnpc fzhjvm nmlwfjzp jpdbf qtgcdr vqb pbn pckrfls nbrntt vjvr xfznh rncvwwqq mhdmtcc ffvnk slw grzxqblp ngfw gbmvvzvq qhrh trjbrzp cbzl rgspsq ljjtc rwfnm vkmzlgzc kcmjjd shv xxzv xnmgwfv lmrwzpw qpzlp jzjj pmwqhz pbrfsrg nswcjr wmvczg tljp mwrggrf vss fmblgjv mpfzbnfc prjnll xvhqt gbzg tfhjnc mrpd zdm rhj gvx flwhzqjc dkmk mpr vbf kqfl bhnw gfksm njmgtlm wqhvvfdl xhpxdp zdcrscjs gvqbj xjcf dqdmfcx dwcbkxw nslnvnb wjfmtngs fcrzzgw qhbr gmkkcx pzsqlz csj pwsccx xhpsmr tsglsbh dfmqqscc jwnmzzdc xljfh hllng llswvqf xvjczjqh rwhznhqc mwhj jkz kxg blpwlzv vxzlwz tzdtfs jpdn gqbwkh mqhjxv fxjllq zwnwtx flgp kdhvrrd mqd sslf xnwvvw thjplqgc xtlpxgg wwrpxqf rjtcchpx bjctn sds jdb pwtdlzs tqx jrmn nwjhl slbwz vmrqk kdfkwp dwhbsphh tkwt nmqsm ppb slcmvfvp vtvf djw mjndf cpmn wmfpxmkz hnzmsr fjkvvx thn hnvdt wtgpkv rgs qcdtjqqd jccdspqd rbkhmz hshmrpg xkjnccwt qrt fkfs bfdwt xsdnh bfdhjthj csk wksvnnf pbnj mczhlf gpnqtfbd zwwpz zvn rthljf ltw stgsds gcjkqn rdgtf pmcwsnnr bdwmdh rgtcs lqdrhbpt zmprxdms nxzmdx slpgfx cpltlszl zwrfsp pqrphcgm htwpkmn kwpntvvg nrfkdfx ksxz zfvx qzgppkd lfr mqwz tvcvgpqh rrbjf fnrvbnm qmsv zkhvcdmg kkfbpng smhqq qtpqlkft slgbx wkvxwcm kgn mttrtqqs tjdtt rjrfsgzl xknsclbk mcbvdcnx rtgld wbbddlc gqmnpzzp gsxdnwqt prmkx ngj mpbxq zlzwhd wgsnv rcp jsk ckblv tfmsnv djlqn btrj cvxgjqh kvw shk hrfcp ktthmtw zdt nlggcz gwxlzgfw xtkvx rhcxr dlnn zbbjqqh rjnvgs psvsmt wskt bfsvwk txj brskv fbwcgm kbbpbf rmnpt tnvrmdk wnslrsfw lfbcg pxt mnwk dcpvcwq xbpgzfhn gpjp lsfbr sdgdxhq pjltpnvh vtjps btqgwnx bkpjtxxm zgjfr cbttjszl zlgwfhsw klf zpgrx zbfxbrzr msm hrhfgfd bpmx xmnrh ncnhxnwk gtzn fdsbb xbrtsjv ltsxscx bvbmnnwp gblnr ljcv sqnkt skrh svb jchd bqvsd ljx jgjdb fxlkmcvj tvglz qkjkdld bqk bsl vdhknm djjdb hppprkz bdfccz dqqp wsmw cvvn dbnqxf dxzvtsc zqwqds nqdtvvpl mqcs bmsmq smthjb pplbtrw lpmddf hrsjfdzp hbpbb mgpnspj rwz pvgq psf cdvrl jmwkrt ljhnsql mddwc mcvhvk rnfx xqxfblzh nxf dwhxs pgdt mtcfxhth jtnwbwb zkfnzflb fsvkvpn vsgwhvp xln mbhx vxn bwdwsmr pszmbbkc cxdhlsns fltrkh gmfdrhws pnpczv lxzdlg hmllsbm cddnd jntxtzr qztdgfz npdgdlsf lkmg swsxjms njs vrddwhkh wwzp lcczhhm kdz jjl phbwrbm kkwww zrqqzq tpqb bsdtmhb dtpdfhp mnmp gcv mtvlh cgdrcr gqvdj mxvsn hdmhn wqfr dljnvlzw phtzsmg hqdjc ttm jtpx vkwq zcs nfq fcjkqqc ntqfwvws klckb cmwv tbrh zktqsjf vmxkz ttngcchg pmjnsvjf mhksp qhh gpjr ftnrfrkl xdbxxj wdlfbx phzwfrxr pldbsmh wlljvz gznxd lvw nhpf jxv fhnhs nqczswv gpkxpzqs dztx ncfqfp xkj qpgshfv jsddbs pzfm bzfqmbx dmbkskh dllmlw zsh nzpdw kxslnqn pjlcmcxf jvcgmflg vcc cxbcwz jlkhsvmx dvxndqf kcbllpw zqb rzfw fbwvmcd qns fmqx wqrtmw rzpss bcxg rdjkwd tfkfnj bnqqj cjzpz hxbvlst rtlpt vrcwh qsh nkcrwhq stssffng hbt qrppcch vdkbrgc btr mmjwp fmstk rvjv gjnljlvv vbwxppd rcpql khnvl vmlpxtwb csd rtjh kjllq xsnfw tmrc xfglnt spdxfn kmvxx qtf kpmkjl fxxrchdg tzwl jqwd qjx zmqqbd jmvk vjldrv pqspncnk scmz zjzt dplp hhpt rwldkr mcbnbr kpx hxdz msdvx dbxcr hldpxf djxflkz hdhkfqh vsvzzft xwwxj pbqm ltfcl qnqd gslsk sdkzg ttxcwpc fkmlmqtk';
+
+const texto = txtB.split(' ');
+let novoTexto = [];
+
+for (i = 0; i < texto.length; i++) {
+  const a = 'k b w r q d n f x j m l v h t c g z p s'.split(' ');
+  const an = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20'.split(' ');
+  let temp = texto[i].split('');
+  console.log('Original text: ' + temp);
+  console.log('replaced String: ' + Replaced(temp, a, an));
+
+  temp = temp.sort(function (a, b) {
+    return a - b;
+  });
+
+  console.log(`sorted String: ' ${temp}`);
+   console.log('reconvertString: ' + Replaced(temp, an, a) + '\n');
+   novoTexto.push(temp);
+   novoTexto.sort();}
+
+   console.log('novo texto: ' + novoTexto.toString());
+
+function Replaced(val, org, rep) {
+  for (let x = 0; x < val.length; x++) {
+    let i = 0;
+    while (i < 20) {
+      //val[x] = val[x].replace(org[i], rep[i]);
+      if (val[x] === org[i]) val[x] = rep[i];
+      i++;
+    }
+  }
+  return val;
+}
+
+//temp = temp.toString();
+//console.log('teste: ' + temp.split(','));
+/* 
+  for (b = 0; b < temp.length; b++) {
+    temp[x] = temp[x].replace('1', 'k');
+    temp[x] = temp[x].replace('2', 'b');
+    temp[x] = temp[x].replace('3', 'w');
+    temp[x] = temp[x].replace('4', 'r');
+    temp[x] = temp[x].replace('5', 'q');
+    temp[x] = temp[x].replace('6', 'd');
+    temp[x] = temp[x].replace('7', 'n');
+    temp[x] = temp[x].replace('8', 'f');
+    temp[x] = temp[x].replace('9', 'x');
+    temp[x] = temp[x].replace('10', 'j');
+    temp[x] = temp[x].replace('11', 'm');
+    temp[x] = temp[x].replace('12', 'l');
+    temp[x] = temp[x].replace('13', 'v');
+    temp[x] = temp[x].replace('14', 'h');
+    temp[x] = temp[x].replace('15', 't');
+    temp[x] = temp[x].replace('16', 'c');
+    temp[x] = temp[x].replace('17', 'g');
+    temp[x] = temp[x].replace('18', 'z');
+    temp[x] = temp[x].replace('19', 'p');
+    temp[x] = temp[x].replace('20', 's');
+  } */
+
+/* 
+  temp = temp.replace(1, 'k');
+  temp = temp.replace(2, 'b');
+  temp = temp.replace(3, 'w');
+  temp = temp.replace(4, 'r');
+  temp = temp.replace(5, 'q');
+  temp = temp.replace(6, 'd');
+  temp = temp.replace(7, 'n');
+  temp = temp.replace(8, 'f');
+  temp = temp.replace(9, 'x');
+  temp = temp.replace(10, 'j');
+  temp = temp.replace(11, 'm');
+  temp = temp.replace(12, 'l');
+  temp = temp.replace(13, 'v');
+  temp = temp.replace(14, 'h');
+  temp = temp.replace(15, 't');
+  temp = temp.replace(16, 'c');
+  temp = temp.replace(17, 'g');
+  temp = temp.replace(18, 'z');
+  temp = temp.replace(19, 'p');
+  temp = temp.replace(20, 's'); */
+
+/* 
+  for (x = 0; x < temp.length; x++) {
+    temp[x] = temp[x].replace('k', '1');
+    temp[x] = temp[x].replace('b', '2');
+    temp[x] = temp[x].replace('w', '3');
+    temp[x] = temp[x].replace('r', '4');
+    temp[x] = temp[x].replace('q', '5');
+    temp[x] = temp[x].replace('d', '6');
+    temp[x] = temp[x].replace('n', '7');
+    temp[x] = temp[x].replace('f', '8');
+    temp[x] = temp[x].replace('x', '9');
+    temp[x] = temp[x].replace('j', '10');
+    temp[x] = temp[x].replace('m', '11');
+    temp[x] = temp[x].replace('l', '12');
+    temp[x] = temp[x].replace('v', '13');
+    temp[x] = temp[x].replace('h', '14');
+    temp[x] = temp[x].replace('t', '15');
+    temp[x] = temp[x].replace('c', '16');
+    temp[x] = temp[x].replace('g', '17');
+    temp[x] = temp[x].replace('z', '18');
+    temp[x] = temp[x].replace('p', '19');
+    temp[x] = temp[x].replace('s', '20');
+  } */
+/* 
+  temp = temp.replace('k', '1 ');
+  temp = temp.replace('b', '2 ');
+  temp = temp.replace('w', '3 ');
+  temp = temp.replace('r', '4 ');
+  temp = temp.replace('q', '5 ');
+  temp = temp.replace('d', '6 ');
+  temp = temp.replace('n', '7 ');
+  temp = temp.replace('f', '8 ');
+  temp = temp.replace('x', '9 ');
+  temp = temp.replace('j', '10 ');
+  temp = temp.replace('m', '11 ');
+  temp = temp.replace('l', '12 ');
+  temp = temp.replace('v', '13 ');
+  temp = temp.replace('h', '14 ');
+  temp = temp.replace('t', '15 ');
+  temp = temp.replace('c', '16 ');
+  temp = temp.replace('g', '17 ');
+  temp = temp.replace('z', '18 ');
+  temp = temp.replace('p', '19 ');
+  temp = temp.replace('s', '20 ');  */
