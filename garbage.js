7f8e4a18f4fcde1cc44a6c9415114467651277c7
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-02-24 04:34:43 -0300

    create fun Replaced in tester.js

---
+++ garbage.js
@@ -0,0 +1,92 @@
+//temp = temp.toString();
+//console.log('teste: ' + temp.split(','));
+/* 
+  for (b = 0; b < temp.length; b++) {
+    temp[x] = temp[x].replace('1', 'k');
+    temp[x] = temp[x].replace('2', 'b');
+    temp[x] = temp[x].replace('3', 'w');
+    temp[x] = temp[x].replace('4', 'r');
+    temp[x] = temp[x].replace('5', 'q');
+    temp[x] = temp[x].replace('6', 'd');
+    temp[x] = temp[x].replace('7', 'n');
+    temp[x] = temp[x].replace('8', 'f');
+    temp[x] = temp[x].replace('9', 'x');
+    temp[x] = temp[x].replace('10', 'j');
+    temp[x] = temp[x].replace('11', 'm');
+    temp[x] = temp[x].replace('12', 'l');
+    temp[x] = temp[x].replace('13', 'v');
+    temp[x] = temp[x].replace('14', 'h');
+    temp[x] = temp[x].replace('15', 't');
+    temp[x] = temp[x].replace('16', 'c');
+    temp[x] = temp[x].replace('17', 'g');
+    temp[x] = temp[x].replace('18', 'z');
+    temp[x] = temp[x].replace('19', 'p');
+    temp[x] = temp[x].replace('20', 's');
+  } */
+
+/* 
+  temp = temp.replace(1, 'k');
+  temp = temp.replace(2, 'b');
+  temp = temp.replace(3, 'w');
+  temp = temp.replace(4, 'r');
+  temp = temp.replace(5, 'q');
+  temp = temp.replace(6, 'd');
+  temp = temp.replace(7, 'n');
+  temp = temp.replace(8, 'f');
+  temp = temp.replace(9, 'x');
+  temp = temp.replace(10, 'j');
+  temp = temp.replace(11, 'm');
+  temp = temp.replace(12, 'l');
+  temp = temp.replace(13, 'v');
+  temp = temp.replace(14, 'h');
+  temp = temp.replace(15, 't');
+  temp = temp.replace(16, 'c');
+  temp = temp.replace(17, 'g');
+  temp = temp.replace(18, 'z');
+  temp = temp.replace(19, 'p');
+  temp = temp.replace(20, 's'); */
+
+/* 
+  for (x = 0; x < temp.length; x++) {
+    temp[x] = temp[x].replace('k', '1');
+    temp[x] = temp[x].replace('b', '2');
+    temp[x] = temp[x].replace('w', '3');
+    temp[x] = temp[x].replace('r', '4');
+    temp[x] = temp[x].replace('q', '5');
+    temp[x] = temp[x].replace('d', '6');
+    temp[x] = temp[x].replace('n', '7');
+    temp[x] = temp[x].replace('f', '8');
+    temp[x] = temp[x].replace('x', '9');
+    temp[x] = temp[x].replace('j', '10');
+    temp[x] = temp[x].replace('m', '11');
+    temp[x] = temp[x].replace('l', '12');
+    temp[x] = temp[x].replace('v', '13');
+    temp[x] = temp[x].replace('h', '14');
+    temp[x] = temp[x].replace('t', '15');
+    temp[x] = temp[x].replace('c', '16');
+    temp[x] = temp[x].replace('g', '17');
+    temp[x] = temp[x].replace('z', '18');
+    temp[x] = temp[x].replace('p', '19');
+    temp[x] = temp[x].replace('s', '20');
+  } */
+/* 
+  temp = temp.replace('k', '1 ');
+  temp = temp.replace('b', '2 ');
+  temp = temp.replace('w', '3 ');
+  temp = temp.replace('r', '4 ');
+  temp = temp.replace('q', '5 ');
+  temp = temp.replace('d', '6 ');
+  temp = temp.replace('n', '7 ');
+  temp = temp.replace('f', '8 ');
+  temp = temp.replace('x', '9 ');
+  temp = temp.replace('j', '10 ');
+  temp = temp.replace('m', '11 ');
+  temp = temp.replace('l', '12 ');
+  temp = temp.replace('v', '13 ');
+  temp = temp.replace('h', '14 ');
+  temp = temp.replace('t', '15 ');
+  temp = temp.replace('c', '16 ');
+  temp = temp.replace('g', '17 ');
+  temp = temp.replace('z', '18 ');
+  temp = temp.replace('p', '19 ');
+  temp = temp.replace('s', '20 ');  */
